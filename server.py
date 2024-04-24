e837f5e32eeb976d925497b88c3889307cdbd9db
Izak Halseide <ihals@colostate.edu>
2024-04-24 09:17:33 -0600

    make server use NetMessage common code

--- server.py
+++ server.py
@@ -6,9 +6,7 @@ Battleship game server
 
 import socket
 import Battleship as bs
-
-# The size in bytes of the length field when sending messages.
-PREFIX_LENGTH: int = 5
+from NetMessage import *
 
 def check_board(board: str) -> bool:
     '''
@@ -17,55 +15,6 @@ def check_board(board: str) -> bool:
     This currently just accepts any board
     '''
     return True
-
-def message_send(sock: socket.socket, message: str, do_log=True):
-    '''
-    Send a length-prefixed message string to the socket connection.
-    This function is the counterpart to `message_recv`.
-    Messages are in the form: [length field][data field]
-    - where the length field is a fixed-length number string (like "00009")
-    - where the data field is a string with length given by converting the length field to an integer
-    '''
-    assert(PREFIX_LENGTH == 5)
-    length = "{:0>5}".format(len(message))
-    if do_log: print(f'(message_send)"{length}{message}"')
-    sock.sendall(length.encode())
-    sock.sendall(message.encode())
-
-def message_recv(sock: socket.socket, do_log=True) -> str:
-    '''
-    Receive a length-prefixed message string from the socket connection.
-    This function is the counterpart to `message_send`.
-    Messages are in the form: [length field][data field]
-    - where the length field is a fixed-length number string (like "00009")
-    - where the data field is a string with length given by converting the length field to an integer
-    '''
-    if do_log: 
-        print("(message_recv)...")
-    try:
-        length_bytes = sock.recv(PREFIX_LENGTH)
-        length_field = length_bytes.decode()
-    except OSError:
-        raise ValueError("could not receive response from connection")
-    if len(length_field) == 0:
-        raise ValueError("connection is closed")
-    if (l := len(length_field)) != PREFIX_LENGTH:
-        raise ValueError(f"the connection sent a length field which itself has an unexpected length of {l}")
-    try:
-        length_num = int(length_field)
-    except ValueError:
-        raise ValueError("the connection sent a length field which could not be converted to an integer value")
-    if length_num <= 0:
-        raise ValueError(f"the connection sent a non-positive integer in the length field: {length_num}")
-    if do_log: 
-        print(f"(message_recv){length_field}...")
-    try:
-        data_field = sock.recv(length_num, socket.MSG_WAITALL).decode()
-    except OSError:
-        raise ValueError("could not receive the full data response from the connection")
-    if (actual_length := len(data_field)) != length_num:
-        raise ValueError(f"connection indicated it would send {length_num} bytes, but {actual_length} bytes was actually received")
-    return data_field
 
 def accept_board(client_sock: socket.socket):
     '''
