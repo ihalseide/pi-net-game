ea4550807de8c0e11ab27df2822814f8b0ca7ee1
Izak Halseide <ihals@colostate.edu>
2024-04-28 20:22:21 -0600

    make a nice user-interactive ship placement process and make the client get the server's outcome for a move

--- client.py
+++ client.py
@@ -11,6 +11,31 @@ import socket
 
 ## File path of where to store a IP address and port information for a connection.
 ADDRESS_FILE_PATH: str = "server.txt"
+
+## Unoccupied tile value.
+UNOCCUPIED = '0'
+
+# Collection of tuples where each entry is:
+# (length, name, character) for the ship
+STANDARD_SHIPS = (
+    (2, 'destroyer', '2'), 
+    (3, 'submarine', '3'), 
+    (3, 'cruiser', bs.monospace_digit_three), 
+    (4, 'battleship', '4'), 
+    (5, 'carrier', '5'),
+)
+
+def print_my_board(personalGameBoard: list[str]) -> None:
+    game_board_str = '=' * 22 + '\n'
+    game_board_str += '        Your Board        |\n'
+    game_board_str += "   " + ' '.join(str(i) for i in range(1, 11)) + '   |\n'
+    
+    for i in range(0, len(personalGameBoard), 10):
+        j = int(i / 10)
+        left_col = ' '.join(map(str, personalGameBoard[i:i+10]))
+        game_board_str += f"{chr(65 + j)}  {left_col}    |\n"
+    
+    print(game_board_str)
     
 def message_send_join(sock: socket.socket, board: str):
     '''
@@ -127,7 +152,8 @@ def input_port() -> int:
             continue
         return port_num
 
-def client_connect_server() -> socket.socket:
+def client_connect_server(board: list[str]) -> socket.socket:
+    board_str = ''.join(board)
     while True:
         # Loop to forever keep getting server addresses to try and join.
         try:
@@ -137,7 +163,7 @@ def client_connect_server() -> socket.socket:
         except (KeyboardInterrupt, EOFError):
             print("\nCancelled.")
             exit(1)
-        message_send_join(sock, bs.convertGameBoardToString(bs.personalGameBoard))
+        message_send_join(sock, board_str)
         response = message_recv(sock)
         if response is None:
             ## Got no response, try again.
@@ -154,17 +180,31 @@ def client_connect_server() -> socket.socket:
             sock.close()
             continue
 
-def client_game_loop(sock: socket.socket) -> None:
+def client_game_loop(sock: socket.socket, board: list[str]) -> None:
     '''
     Main client game loop to keep sending moves whenever it is this client's turn.
     '''
+    hit_miss_board = [ '~' for i in range(100) ]
+    move_index = -1
     while True:
+        print("Waiting for your turn...")
         msg = message_recv(sock)
         if msg == MSG_MY_TURN:
             ## Server sent that it is our turn to go
+            print(bs.createPrintableGameBoard(board, hit_miss_board))
             move = get_user_move()
-            print(f"move: {move}")
+            move_index = bs.returnMoveIndex(move)
+            print(f"move: {move}, index: {move_index}")
             send_move(sock, move)
+            # get response in next loop (hit/miss)
+            continue
+        elif msg == f"{MSG_OUTCOME} hit":
+            ## Previously sent move was a hit
+            hit_miss_board[move_index] = 'X'
+        elif msg == f"{MSG_OUTCOME} miss":
+            ## Previously sent move was a miss
+            assert(move_index >= 0)
+            hit_miss_board[move_index] = '.'
         elif msg.startswith(MSG_FINISHED):
             ## Server is ending/finishing the game
             ## Message is: "finish " followed by the outcome
@@ -183,12 +223,119 @@ def client_game_loop(sock: socket.socket) -> None:
             print(f"Unhandled server message type.")
             print(f"Received server data: '{msg}'")
 
+def prompt_valid_board_location(board: list[str]) -> int:
+    while True:
+        x = input("\rEnter location for the ship's front (A1 through I10): ")
+        try:
+            index: int = bs.returnMoveIndex(x)
+        except ValueError:
+            print("\rCannot start placing a boat there: invalid location")
+            continue
+        if board[index] != UNOCCUPIED:
+            print("\rCannot start placing a boat there: spot out of range or already occupied")
+            continue
+        return index
+
+def prompt_board_direction() -> str:
+    while True:
+        d = input("Enter direction for the ship to face, starting from the previous location. (Up, Down, Left, or Right): ")
+        if not d:
+            print("Cannot be empty")
+            continue
+        d = d.lower()[0]
+        if d in 'udlr':
+            return d
+        else:
+            print("Please enter u[p], d[own], l[eft], or r[ight]")
+
+def index_to_row_col(index: int) -> tuple[int, int]:
+    if index > 100:
+        raise ValueError("index out of range")
+    r = index // 10
+    c = index % 10
+    return r, c
+
+def row_col_to_index(row: int, col: int) -> int:
+    return row * 10 + col
+
+def row_col_to_coord(row: int, col: int) -> str:
+    return 'ACDEFGHIJ'[row] + str(col)
+
+def direction_name(direction: str) -> str:
+    names = {
+        'u': 'up',
+        'd': 'down',
+        'l': 'left',
+        'r': 'right',
+    }
+    return names[direction]
+
+def direction_to_delta(direction: str) -> tuple[int, int]:
+    # maps directions to delta_row, delta_column
+    row_col_delta = {
+        'u': (-1, 0),
+        'd': (1, 0),
+        'l': (0, -1),
+        'r': (0, 1),
+    }
+    return row_col_delta[direction]
+
+def prompt_valid_board_direction(board: list[str], front_loc: int, length: int) -> str:
+    while True:
+        d_in = prompt_board_direction()
+        row_0, col_0 = index_to_row_col(front_loc)
+        delta = direction_to_delta(d_in)
+        valid = True
+        for i in range(length):
+            row = row_0 + i * delta[0]
+            col = col_0 + i * delta[1]
+            if board[row_col_to_index(row, col)] != UNOCCUPIED:
+                valid = False
+                coord = row_col_to_coord(row, col)
+                direction = direction_name(d_in)
+                print(f"Ship cannot be layed out in the {direction} direction because there is an obstacle at {coord}")
+                break
+        if valid:
+            return d_in
+
+def set_ship_squares(board: list[str], front_loc: int, direction: str, length: int, ship_value: str):
+    row_0, col_0 = index_to_row_col(front_loc)
+    delta = direction_to_delta(direction)
+    for i in range(length):
+        row = row_0 + i * delta[0]
+        col = col_0 + i * delta[1]
+        board[row_col_to_index(row, col)] = ship_value
+
+def player_setup_board(ships_info_tuple: tuple[tuple[int, str, str], ...]) -> list[str]:
+    board = [ UNOCCUPIED for i in range(100) ]
+    dummy_hit_miss = [ ' ' for i in range(100) ]
+    for (ship_length, ship_name, ship_value) in ships_info_tuple:
+        print_my_board(board)
+        print(f"Place your {ship_name} (occupies {ship_length} tiles)")
+        # Loop and catch Ctrl-C for direction choice to allow the user to re-choose the 'front_loc' location
+        # in case there is no valid direction for a chosen 'front_loc'.
+        while True:
+            front_loc = prompt_valid_board_location(board)
+            try:
+                direction = prompt_valid_board_direction(board, front_loc, ship_length)
+                break
+            except (KeyboardInterrupt, EOFError):
+                print(f"Undoing the starting position for the {ship_name}...")
+                continue
+        set_ship_squares(board, front_loc, direction, ship_length, ship_value)
+    print(bs.createPrintableGameBoard(board, dummy_hit_miss))
+    return board
+
 def client_main() -> None:
     print("Welcome to the game client")
-    sock = client_connect_server()
+    try:
+        board = player_setup_board(STANDARD_SHIPS)
+    except (KeyboardInterrupt, EOFError):
+        print("Board set-up cancelled, so the game will not continue.")
+        return
+    sock = client_connect_server(board)
     print(f"Joined server!")
-    bs.setupGamePieces()
-    client_game_loop(sock)
+    client_game_loop(sock, board)
     sock.close()
     print("Goodbye from the game client")
 
